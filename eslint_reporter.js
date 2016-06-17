18e17961933f2096f7a5fa8560b02b8fb97e6350
Developer <>
2016-06-16 22:02:21 -0400

    fix some issues with the build system

--- eslint_reporter.js
+++ eslint_reporter.js
@@ -1,62 +1,64 @@
-module.exports = function (results) {
-	var results = results || [];
+module.exports = function( results ) {
+	results = results || [];
 	var errors = 0;
 	var warnings = 0;
 	var tooMany = false;
 	var line;
 
-	results.some(function (current) {
-		current.messages.some(function (msg) {
+	results.some(function( current ) {
+		current.messages.some(function( msg ) {
 			line = current.filePath;
 
-			line+= ':' + (msg.line || 0) + ':' + (msg.column || 0) + ': ';
+			line += ":" + (msg.line || 0) + ":" + (msg.column || 0) + ": ";
 
-			if (msg.severity === 1) {
-				line += 'warning';
+			if ( msg.severity === 1 ) {
+				line += "warning";
 				warnings++;
-			}
-			else if (msg.severity === 2) {
-				line += 'error';
+			} else if ( msg.severity === 2 ) {
+				line += "error";
 				errors++;
 			}
 
-			line += ': ' + msg.ruleId + ': ' + msg.message;
+			line += ": " + msg.ruleId + ": " + msg.message;
 
-			console.log(line);
+			console.log( line );
 
-			if (errors === 20)
+			if ( errors === 20 ) {
 				tooMany = true;
+			}
 
 			return tooMany;
 		});
 		return tooMany;
 	});
 
-	if (errors > 0 || warnings > 0) {
-		line = '';
+	if ( errors > 0 || warnings > 0 ) {
+		line = "";
 
-		if (tooMany)
-			line = 'fatal error: too many errors emitted, stopping now.\n';
+		if ( tooMany ) {
+			line = "fatal error: too many errors emitted, stopping now.\n";
+		}
 
-		if (warnings > 0) {
-			line += warnings +' warning';
-			if (warnings > 1) {
-				line += 's';
+		if ( warnings > 0 ) {
+			line += warnings + " warning";
+			if ( warnings > 1 ) {
+				line += "s";
 			}
 		}
 
-		if (errors > 0 && warnings > 0)
-			line += ' and ';
+		if ( errors > 0 && warnings > 0 ) {
+			line += " and ";
+		}
 
-		if (errors > 0) {
-			line += errors +' error';
-			if (errors > 1) {
-				line += 's';
+		if ( errors > 0 ) {
+			line += errors + " error";
+			if ( errors > 1 ) {
+				line += "s";
 			}
 		}
 
-		line += ' generated.';
+		line += " generated.";
 
-		console.log(line);
+		console.log( line );
 	}
 };
--- jscs_reporter.js
+++ jscs_reporter.js
@@ -1,8 +1,8 @@
-module.exports = function(results) {
-	results.forEach(function(current) {
-		if (!current.isEmpty()) {
-			current.getErrorList().forEach(function(msg){
-				console.log(current.getFilename() + ':' + msg.line + ':' + msg.column + ': warning: ' + msg.message);
+module.exports = function( results ) {
+	results.forEach(function( current ) {
+		if ( !current.isEmpty() ) {
+			current.getErrorList().forEach(function( msg ) {
+				console.log( current.getFilename() + ":" + msg.line + ":" + msg.column + ": warning: " + msg.message );
 			});
 		}
 	});
