9a0e6433647c7a63fbe6e60789a810010214ef01
Developer <>
2016-06-30 12:06:38 -0400

    move the call to register the component to the flow-main.js

--- flow-main.js
+++ flow-main.js
@@ -19,70 +19,6 @@ var play = false;
 var socket;
 var localSocket = {};
 
-function init() {
-	canvDiv = document.getElementById("container" );
-
-	if ( document.io ) {
-		socket = io.connect( "http://login.sccs.swarthmore.edu:8201" );
-	}
-
-
-	makeTypes();
-
-	// window.setInterval(step,1000/sps);
-	// t0=Date.now()/1000;
-	step();
-
-	// window.setInterval(draw,1000/fps);
-
-	// this is supposed to be better.
-	(function animloop() {
-      requestAnimFrame( animloop );
-      draw();
-    })();
-
-
-	var tmpScene = localStorage.getItem("tmpScene" );
-	if ( tmpScene ) {
-	   loadScene( JSON.parse( tmpScene ) );
-	}
-
-	/*
-	logger=new nodeTypes.Logger({x:500,y:200});
-	summer=new nodeTypes.Summer({x:200,y:100});
-	mou=new nodeTypes.Mouse( {x:100,y:180});
-	tim=new nodeTypes.Time(  {x: 50,y:410});
-	sin1=new nodeTypes.Sine( {x: 50,y:300,i:{freq:.5}});
-	sin2=new nodeTypes.Sine( {x:200,y:300,i:{freq:4.05}});
-	dis=new nodeTypes.Scope( {x:500,y:350});
-	cus=new nodeTypes.Custom({x:200,y:400});
-	t2=new nodeTypes.X2(	 {x:350,y:150});
-
-
-	mkWire(sin1,'y',sin2,'amp',100);
-	mkWire(sin2,'y',dis,'y1',220);
-
-	mkWire(summer,'c',t2,'a',0);
-	mkWire(t2,'b',logger,'msg',100);
-
-	mkWire(tim,'t',cus,'a',320);
-	mkWire(tim,'t',dis,'x',320);
-
-	mkWire(cus,'x',dis,'y2',0);
-	mkWire(cus,'y',dis,'y3',100);
-   // mkWire(lfo,'y',dis,'y1',220);
-
-	*/
-
-	makeLibrary();
-
-// 	canvDiv.onmousedown=function(e){
-// 		console.log(e);
-// 	}
-
-}
-
-
 var draw = function draw() {
 	// if ( play ) {
 		// step();
@@ -110,10 +46,6 @@ var step = function step() {
 
 	window.setTimeout( step, 1000 / sps );
 };
-
-function unloading() {
-	localStorage.setItem("tmpScene", JSON.stringify( exportNodes( nodes ) ) );
-}
 
 // misc utility funcs
 
@@ -144,3 +76,78 @@ window.requestAnimFrame = (function() {
       window.setTimeout( callback, 1000 / 60 );
     };
 })();
+
+Polymer({
+    is: 'hello-world',
+    properties: {
+        who: {
+            type: String,
+            value: 'World'
+        }
+    },
+	ready: function(){
+		canvDiv = document.getElementById("container" );
+
+		if ( document.io ) {
+			socket = io.connect( "http://login.sccs.swarthmore.edu:8201" );
+		}
+
+
+		makeTypes();
+
+		// window.setInterval(step,1000/sps);
+		// t0=Date.now()/1000;
+		step();
+
+		// window.setInterval(draw,1000/fps);
+
+		// this is supposed to be better.
+		(function animloop() {
+		  requestAnimFrame( animloop );
+		  draw();
+		})();
+
+
+		var tmpScene = localStorage.getItem("tmpScene" );
+		if ( tmpScene ) {
+		   loadScene( JSON.parse( tmpScene ) );
+		}
+
+		/*
+		logger=new nodeTypes.Logger({x:500,y:200});
+		summer=new nodeTypes.Summer({x:200,y:100});
+		mou=new nodeTypes.Mouse( {x:100,y:180});
+		tim=new nodeTypes.Time(  {x: 50,y:410});
+		sin1=new nodeTypes.Sine( {x: 50,y:300,i:{freq:.5}});
+		sin2=new nodeTypes.Sine( {x:200,y:300,i:{freq:4.05}});
+		dis=new nodeTypes.Scope( {x:500,y:350});
+		cus=new nodeTypes.Custom({x:200,y:400});
+		t2=new nodeTypes.X2(	 {x:350,y:150});
+
+
+		mkWire(sin1,'y',sin2,'amp',100);
+		mkWire(sin2,'y',dis,'y1',220);
+
+		mkWire(summer,'c',t2,'a',0);
+		mkWire(t2,'b',logger,'msg',100);
+
+		mkWire(tim,'t',cus,'a',320);
+		mkWire(tim,'t',dis,'x',320);
+
+		mkWire(cus,'x',dis,'y2',0);
+		mkWire(cus,'y',dis,'y3',100);
+	   // mkWire(lfo,'y',dis,'y1',220);
+
+		*/
+
+		makeLibrary();
+
+	// 	canvDiv.onmousedown=function(e){
+	// 		console.log(e);
+	// 	}
+
+	},
+	detached: function unloading(){
+		localStorage.setItem("tmpScene", JSON.stringify( exportNodes( nodes ) ) );
+	}
+});
